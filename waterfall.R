dd542ee82eb924ed3914142c52c8db3d9f1941f0
Zhicheng Ji <zhichengji@gmail.com>
2016-02-11 20:47:50 -0500

    update data

--- waterfall.R
+++ waterfall.R
@@ -1,14 +1,29 @@
 library(monocle)
 library(data.table)
 
-###HSMM rawdata
+###HSMM with prior information
 data(HSMM)
 HSMMdata <- exprs(HSMM)
 HSMMdata <- log2(HSMMdata+1)
 colnames(HSMMdata) <- c(paste0("T1_",1:sum(grepl("T0",colnames(HSMMdata)))),paste0("T2_",1:sum(grepl("T24",colnames(HSMMdata)))),paste0("T3_",1:sum(grepl("T48",colnames(HSMMdata)))),paste0("T4_",1:sum(grepl("T72",colnames(HSMMdata)))))
-save(HSMMdata,file="paper/HSMM/HSMMdata.rda")
+data <- HSMMdata
+aggdata <- data[fData(HSMM)$use_for_ordering,]
+HSMMwaterfallorder <- pseudotimeprog.foo(aggdata,color="black") 
 
-###LPS rawdata
+###HSMM without prior information
+data(HSMM)
+HSMMdata <- exprs(HSMM)
+HSMMdata <- log2(HSMMdata+1)
+colnames(HSMMdata) <- c(paste0("T1_",1:sum(grepl("T0",colnames(HSMMdata)))),paste0("T2_",1:sum(grepl("T24",colnames(HSMMdata)))),paste0("T3_",1:sum(grepl("T48",colnames(HSMMdata)))),paste0("T4_",1:sum(grepl("T72",colnames(HSMMdata)))))
+data <- HSMMdata
+data <- data[rowSums(data) > 0,]
+clures <- hclust(dist(data))
+cluster <- cutree(clures,0.05*nrow(data))
+aggdata <- aggregate(data,list(cluster),mean)
+aggdata <- aggdata[,-1]
+HSMMwaterfallorder <- pseudotimeprog.foo(aggdata,color="black")  
+
+###LPS 
 rawdata <- fread("paper/rawdata/GSE48968_allgenesTPM_GSM1189042_GSM1190902.txt",header=T)
 rn <- as.matrix(rawdata[,1,with=F])
 rawdata <- as.matrix(rawdata[,-1,with=F])
@@ -21,9 +36,21 @@ LPSdata6 <- as.matrix(rawdata[,grep("^LPS_6h_S[0-9]*$",colnames(rawdata))])
 LPSdata <- cbind(LPSdata1,LPSdata2,LPSdata4,LPSdata6)
 LPSdata <- log2(LPSdata + 1)
 colnames(LPSdata) <- c(paste0("T1_",1:sum(grepl("^LPS_1h_S[0-9]*$",colnames(LPSdata)))),paste0("T2_",1:sum(grepl("^LPS_2h_S[0-9]*$",colnames(LPSdata)))),paste0("T3_",1:sum(grepl("^LPS_4h_S[0-9]*$",colnames(LPSdata)))),paste0("T4_",1:sum(grepl("^LPS_6h_S[0-9]*$",colnames(LPSdata)))))
-save(LPSdata,file="paper/LPS/LPSdata.rda")
+data <- LPSdata[rowSums(LPSdata) > 0,]
+clures <- hclust(dist(data))
+cluster <- cutree(clures,0.05*nrow(data))
+aggdata <- aggregate(data,list(cluster),mean)
+aggdata <- aggdata[,-1]
+LPSwaterfallorder <- pseudotimeprog.foo(aggdata,color="black")
 
-###neural rawdata
+
+
+###neural 
 neuraldata <- read.table("paper/rawdata/Single_TPM.txt",as.is=T,sep="\t",row.names = 1)
 neuraldata <- as.matrix(log2(neuraldata+1))
-save(neuraldata,file="paper/neural/neuraldata.rda")
+data <- neuraldata[rowSums(neuraldata) > 0,]
+clures <- hclust(dist(data))
+cluster <- cutree(clures,0.05*nrow(data))
+aggdata <- aggregate(data,list(cluster),mean)
+aggdata <- aggdata[,-1]
+neuralwaterfallorder <- pseudotimeprog.foo(aggdata,color="black")
